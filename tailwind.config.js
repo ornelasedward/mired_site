a425ffa8029c7f7174377d371f4fa22ea5a652ba
Edward Ornelas <ornelasedward@rocketmail.com>
2023-01-05 12:53:45 -0600

    deleted un-used css

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,25 +25,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        page: "#FEF8F5",
-        primary: "#402B2B",
-        accent: {
-          DEFAULT: "#FEB702",
-          hover: "#FFDb81",
-        },
-        tint: "#FDEDE8",
-        darkblue: "#0F264C",
+        accent: {},
       },
-      dropShadow: {
-        primary: "0 20px 40px rgba(238, 77, 71, 0.1)",
-        secondary: "0px 30px 40px rgba(244, 125, 103, 0.2)",
-        tertiary: "0 20px 40px rgba(32, 56, 100, 0.3);",
-      },
-      backgroundImage: {
-        hero: "url('/images/hero/scene.svg')",
-        faq: "url('/images/faq/bg.svg')",
-        footer: "url('/images/footer/bg.svg')",
-      },
+      dropShadow: {},
+      backgroundImage: {},
       animation: {
         spin: "spin 20s linear infinite",
       },
